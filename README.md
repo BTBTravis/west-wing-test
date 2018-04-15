ab192f0514d029024f4fb04f6a299442c9d414e8
Travis Shears <btbtravis@gmail.com>
2018-04-15 09:51:13 -0500

    Add solution3 to README.md

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 ```
 (function () {
   var usersEls = $('.userNode').toArray()
-  if (usersEls.length < 1) return null // if we don't have any users on this page exit
+  if (usersEls.length < 1) return // if we don't have any users on this page exit
 
   // map over the userEls and load info on their users from the api
   usersEls.map(function (userEl) {
@@ -73,7 +73,7 @@ export default class SetStatus {
     this.els = Array.from(document.querySelectorAll('.elements'));
     this.els.map(el => {
       let id = el.getAttribute('data-id');
-      if(!id) return null; // if we don't have an id don't attach click event handler
+      if(!id) return; // if we don't have an id don't attach click event handler
       el.addEventListener('click', () => {
         jquery.ajax({
           url: '/change-status',
@@ -96,6 +96,51 @@ export default class SetStatus {
 
 }
 ```
-3. [Task 3](task3.md) (Writing from scratch)
+## 3. [Task 3](solution3) (Writing from scratch)
 
+
+```
+(function () {
+  var blocks = Array.from(document.querySelectorAll('.expando-block'));
+  if (blocks.length < 1) return;
+  /**
+   * Create an expando-block by adding a btn and class if expando-body content is over 3 lines
+   * @param {HTMLElement} el - div with class expando-block
+   */
+  var createExpandoBlock = function (el) {
+    var init = function () {
+      var body = el.querySelector('.expando-body');
+      var height = body.clientHeight;
+      var maxHeight = 3.4 * parseFloat(getComputedStyle(body).fontSize);
+      if(height > maxHeight) {
+        makeCollapsible(el);
+      }
+    };
+
+    /**
+     * Adds the closed class to find extra content and add a button with toggle event
+     * @param {HTMLElement} el - div with class expando-block
+     */
+    var makeCollapsible = function (el) {
+      var btn = document.createElement("button");// create button
+      var newContent = document.createTextNode("Read More"); // add txt to button
+      btn.appendChild(newContent); // add text to button
+      el.querySelector('.expando-footer').appendChild(btn); // add button to footer
+      el.querySelector('.expando-body').classList.add('closed'); // close the expando-body
+      btn.addEventListener('click', function () {
+        el.querySelector('.expando-body').classList.toggle('closed'); // toggle the expando-body
+        btn.textContent = /read/ig.test(btn.textContent) ? 'Show Less' : 'Read More';
+      });
+    }
+
+    // initlize expando-block
+    init();
+  };
+
+  blocks.map(function (el) {
+    createExpandoBlock(el);
+  });
+
+}());
+```
 4. [Task 4](task4.md) (Writing from scratch with React)
--- solution3/main.js
+++ solution3/main.js
@@ -1,39 +1,42 @@
-var blocks = Array.from(document.querySelectorAll('.expando-block'));
+(function () {
+  var blocks = Array.from(document.querySelectorAll('.expando-block'));
+  if (blocks.length < 1) return;
+  /**
+   * Create an expando-block by adding a btn and class if expando-body content is over 3 lines
+   * @param {HTMLElement} el - div with class expando-block
+   */
+  var createExpandoBlock = function (el) {
+    var init = function () {
+      var body = el.querySelector('.expando-body');
+      var height = body.clientHeight;
+      var maxHeight = 3.4 * parseFloat(getComputedStyle(body).fontSize);
+      if(height > maxHeight) {
+        makeCollapsible(el);
+      }
+    };
 
-/**
- * Create an expando-block by adding a btn and class if expando-body content is over 3 lines
- * @param {HTMLElement} el - div with class expando-block
- */
-var createExpandoBlock = function (el) {
-  var init = function () {
-    var body = el.querySelector('.expando-body');
-    var height = body.clientHeight;
-    var maxHeight = 3.4 * parseFloat(getComputedStyle(body).fontSize);
-    if(height > maxHeight) {
-      makeCollapsible(el);
+    /**
+     * Adds the closed class to find extra content and add a button with toggle event
+     * @param {HTMLElement} el - div with class expando-block
+     */
+    var makeCollapsible = function (el) {
+      var btn = document.createElement("button");// create button
+      var newContent = document.createTextNode("Read More"); // add txt to button
+      btn.appendChild(newContent); // add text to button
+      el.querySelector('.expando-footer').appendChild(btn); // add button to footer
+      el.querySelector('.expando-body').classList.add('closed'); // close the expando-body
+      btn.addEventListener('click', function () {
+        el.querySelector('.expando-body').classList.toggle('closed'); // toggle the expando-body
+        btn.textContent = /read/ig.test(btn.textContent) ? 'Show Less' : 'Read More';
+      });
     }
+
+    // initlize expando-block
+    init();
   };
 
-/**
- * Adds the closed class to find extra content and add a button with toggle event
- * @param {HTMLElement} el - div with class expando-block
- */
-  var makeCollapsible = function (el) {
-    var btn = document.createElement("button");// create button
-    var newContent = document.createTextNode("Read More"); // add txt to button
-    btn.appendChild(newContent); // add text to button
-    el.querySelector('.expando-footer').appendChild(btn); // add button to footer
-    el.querySelector('.expando-body').classList.add('closed'); // close the expando-body
-    btn.addEventListener('click', function () {
-      el.querySelector('.expando-body').classList.toggle('closed'); // toggle the expando-body
-      btn.textContent = /read/ig.test(btn.textContent) ? 'Show Less' : 'Read More';
-    });
-  }
+  blocks.map(function (el) {
+    createExpandoBlock(el);
+  });
 
-  // initlize expando-block
-  init();
-};
-
-blocks.map(function (el) {
-  createExpandoBlock(el);
-});
+}());
